702175fdf457f0bdd52a6b51e8109ed95161cfc8
Sergio Bongiorno <101117801+serbng@users.noreply.github.com>
2025-06-26 15:39:58 +0200

    Update raw_image.py
    Updated mosaic function. Now it's multiprocessed.

--- laueutils/visualization/raw_image.py
+++ laueutils/visualization/raw_image.py
@@ -1,8 +1,10 @@
 import fabio
 import numpy as np
 import matplotlib.pyplot as plt
-from graintools.utils import linear_chunks # for mosaic
-from graintools.visualization._utils import draw_colorbar
+import multiprocess as mp
+
+from ..utils.chunks import linear_chunks # for mosaic
+from ._utils import draw_colorbar
 
 def display_image(data, roi=None, **kwargs):
     """Plot an image.
@@ -20,7 +22,7 @@ def display_image(data, roi=None, **kwargs):
     if isinstance(data, str):
         with fabio.open(data) as f:
             image_data = f.data
-    if isinstance(data, np.ndarray):
+    elif isinstance(data, np.ndarray):
         image_data = data
     else:
         raise TypeError(
@@ -41,8 +43,45 @@ def display_image(data, roi=None, **kwargs):
     ax.set_xlabel('X pixel')
     ax.set_ylabel('Y pixel')
     ax.set_aspect('equal')
+
+def _mosaic_row(row_paths, roi_indices):
+    """
+    Worker function in mosaic.
+
+    Parameters
+    ----------
+    row_paths      [list]: list of paths to the files of the images of one row of the mosaic.
+    roi_indices   [tuple]: (x1, x2, y1, y2) that will be used as a slice of the image data.
+
+    Returns
+    ----------
+    row_data [np.ndarray]: Array of shape (x2-x1, (y2-y1)*len(row_paths))
+    """
+    x1, x2, y1, y2 = roi_indices
+    row_data = []
+    for path in row_paths:
+        # Fetch data whether it exists or not
+        try:
+            with fabio.open(path) as image:
+                image_data = image.data
+            # Cropping
+            image_data = image_data[x1:x2, y1:y2]
+        except(IndexError, IOError):
+            roi_boxsize = (x2-x1, y2-y1)
+            image_data = np.zeros(roi_boxsize)
+
+        row_data.append(image_data)
+        
+    row_data = np.hstack(row_data)
+    # Now row_data is a matrix containing the data of a row
+    # <-- len(row)*roi_boxsize[1] -->
+    # *-----*-----*   .....   *-----*  
+    # |     |     |           |     |  roi_boxsize[0]
+    # |     |     |           |     |  
+    # *-----*-----*   .....   *-----*  
+    return row_data
     
-def mosaic(paths, num_rows, num_cols, roi_center, roi_boxsize, verbose=True):
+def mosaic(paths, num_rows, num_cols, roi_center, roi_boxsize, workers=4):
     y1 = int(roi_center[0] - roi_boxsize[0] // 2)
     y2 = int(roi_center[0] + roi_boxsize[0] // 2)
     x1 = int(roi_center[1] - roi_boxsize[1] // 2)
@@ -56,35 +95,15 @@ def mosaic(paths, num_rows, num_cols, roi_center, roi_boxsize, verbose=True):
     #  ...
     #  [6479, 6480, 6481, 6482, 6483, ..., 6560]]
     #Each row_indices contains the indices of the files corresponding to a row
-    mosaic_data = []
-    for row in row_indices:
-        # I go through each column in the row, and I append a matrix with shape
-        # roi_boxsize[1], roi_boxsize[0] and I concatenate horizontally at the end
-        row_data = []
-        for image_index in row:
-            try:
-                if verbose:
-                    print(f'Opening image {paths[image_index]}', end='\r')
-                try:
-                    with fabio.open(paths[image_index]) as image:
-                        image_data = image.data
-                    # Cropping
-                    image_data = image_data[x1:x2, y1:y2]
-                except IOError:
-                    image_data = np.zeros(roi_boxsize)
-            except IndexError:
-                image_data = np.zeros(roi_boxsize)
-                
-            row_data.append(image_data)
-        row_data = np.hstack(row_data)
-        # Now row_data is a matrix containing the data of a row
-        # <-- len(row)*roi_boxsize[1] -->
-        # *-----*-----*   .....   *-----*  
-        # |     |     |           |     |  roi_boxsize[0]
-        # |     |     |           |     |  
-        # *-----*-----*   .....   *-----*  
-        # I append it to the overall mosaic_data
-        # and at the very end i concatenate vertically 
-        mosaic_data.append(row_data)
+    row_paths = [ [paths[i] for i in columns] for columns in row_indices]
+    with mp.Pool(workers) as pool:
+        mosaic_rows = pool.starmap(
+            lambda row: _mosaic_row(row, (x1, x2, y1, y2)),
+            row_paths,
+            chunksize=1
+        )
+    # mosaic_rows is now a list of arrays containing the data of the rows
+    # concatenating them vertically creates the mosaic
+    mosaic = np.vstack(mosaic_rows)
     
-    return np.vstack(mosaic_data)    
+    return mosaic    
