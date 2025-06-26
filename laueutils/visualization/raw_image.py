ea88832055b1c2d40704d364c247ff6ccadc8c45
Sergio Bongiorno <101117801+serbng@users.noreply.github.com>
2025-06-26 16:47:37 +0200

    Update raw_image.py
    Added docstring to mosaic.

--- laueutils/visualization/raw_image.py
+++ laueutils/visualization/raw_image.py
@@ -11,9 +11,9 @@ def display_image(data, roi=None, **kwargs):
     
     Parameters
     ----------
-    image_path      [str]: Full path to the image
-    ROI        tuple[int]: Subset of pixels inside the image to plot. The format is:
-                           (x_position, y_position, x_boxsize, y_boxsize)
+    image_path        (str): Full path to the image
+    ROI        (tuple[int]): Subset of pixels inside the image to plot. The format is:
+                             (x_position, y_position, x_boxsize, y_boxsize)
     
     Keyword arguments
     ----------
@@ -39,23 +39,22 @@ def display_image(data, roi=None, **kwargs):
     
     ax = plt.gca()
     image = ax.imshow(image_data, **kwargs)
-    draw_colorbar(image, ax)
+    draw_colorbar(image)
     ax.set_xlabel('X pixel')
     ax.set_ylabel('Y pixel')
     ax.set_aspect('equal')
 
 def _mosaic_row(row_paths, roi_indices):
-    """
-    Worker function in mosaic.
+    """Worker function in mosaic.
 
     Parameters
     ----------
-    row_paths      [list]: list of paths to the files of the images of one row of the mosaic.
-    roi_indices   [tuple]: (x1, x2, y1, y2) that will be used as a slice of the image data.
+    row_paths      (list[str]): list of paths to the files of the images of one row of the mosaic.
+    roi_indices   (tuple[int]): (x1, x2, y1, y2) that will be used as a slice of the image data.
 
     Returns
     ----------
-    row_data [np.ndarray]: Array of shape (x2-x1, (y2-y1)*len(row_paths))
+    row_data      [np.ndarray]: Array of shape (x2-x1, (y2-y1)*len(row_paths)) with the data of a row.
     """
     x1, x2, y1, y2 = roi_indices
     row_data = []
@@ -82,6 +81,25 @@ def _mosaic_row(row_paths, roi_indices):
     return row_data
     
 def mosaic(paths, num_rows, num_cols, roi_center, roi_boxsize, workers=4):
+    """Stitch together the same ROI of different images to create a mosaic.
+    
+    The images are stitched together row by row. So, if ´´´num_cols=10´´´, the images are read in chunks of 10 and
+    put in a row. At the end the rows are stacked on top of each other.
+    
+    Parameters
+    ----------
+    paths        (list[str]): List of paths to the images used to build the mosaic.
+    num_rows           (int): When the images come from a 2D scan, number of rows of the scan.
+    num_cols           (int): When the images come from a 2D scan, number of coloumns of the scan.
+    roi_center  (tuple[int]): Position on the detector to track.
+    roi_boxsize (tuple[int]): Size of the ROI. It is the side of a square centered at ´´´roi_center´´´.
+    workers            (int): (optional) Default to 4. Number of cpus to use to speed up the process.
+
+
+    Returns
+    ----------
+    mosaic      (np.ndarray): Array of shape (num_rows*roi_boxsize[1], num_cols*roi_boxsize[0]). Result of the mosaic.
+    """
     y1 = int(roi_center[0] - roi_boxsize[0] // 2)
     y2 = int(roi_center[0] + roi_boxsize[0] // 2)
     x1 = int(roi_center[1] - roi_boxsize[1] // 2)
@@ -94,7 +112,7 @@ def mosaic(paths, num_rows, num_cols, roi_center, roi_boxsize, workers=4):
     #  [  81,   82,   83,   84,   85, ...,  161].
     #  ...
     #  [6479, 6480, 6481, 6482, 6483, ..., 6560]]
-    #Each row_indices contains the indices of the files corresponding to a row
+    # Each row_indices contains the indices of the files corresponding to a row
     row_paths = [ [paths[i] for i in columns] for columns in row_indices]
     with mp.Pool(workers) as pool:
         mosaic_rows = pool.starmap(
