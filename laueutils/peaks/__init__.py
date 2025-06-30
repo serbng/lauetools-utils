6488713d3fbb1c6d4228efbaa7a2679de1b04578
Sergio Bongiorno <101117801+serbng@users.noreply.github.com>
2025-06-30 10:21:07 +0200

    Update __init__.py
    Added imports from indexation.py

--- laueutils/peaks/__init__.py
+++ laueutils/peaks/__init__.py
@@ -3,3 +3,4 @@ from .remove_duplicates import remove_duplicates
 from .subtract import subtract
 from .track import track
 from .simulate import simulate
+from .indexation import index, check_orientation
